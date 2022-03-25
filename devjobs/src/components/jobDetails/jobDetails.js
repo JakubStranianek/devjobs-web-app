fa0541841261e1ec37078a23f7f66487bd1febb3
JakubStranianek <j.stranianek@gmail.com>
2022-03-25 11:19:06 +0100

    link to concrete job

--- devjobs/src/components/jobDetails/jobDetails.js
+++ devjobs/src/components/jobDetails/jobDetails.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
-export default function jobDetails() {
+import {useParams} from "react-router-dom"
+import data from "../mainJobs/data.json"
+import "./jobDetails.css"
+import { Link } from 'react-router-dom'
+
+export default function JobDetails() {
+  const {position} = useParams()
+  const job = data.find(job => job.position === position)
+
   return (
-    <div>jobDetails</div>
+    <div className='jobDetailsFlexxer'>
+      <section className='headerJobDetails'> 
+        <div style={{backgroundColor: job.logoBackground}} className='jobDetailsLogo'>
+          <img src={job.logo} alt="jobLogoDetail"></img>
+        </div>
+
+      <div className='textAndButtonFlex'>
+        <div className='headerTextWrapper'>
+          <h6>{job.company}</h6>
+          <p>{job.company.toLowerCase() + ".com"}</p>
+        </div>
+
+      <div id='linkBg'>
+        <Link to={job.website} onClick={e => e.preventDefault()}>Company Site</Link>
+      </div>
+
+      </div>
+      </section>
+    
+    
+      {/* <section className='jobDetailsMain'>
+
+      </section> */}
+    
+    </div>
+    
   )
 }
